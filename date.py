0c919f5a271951dca685c186663095a7f33c12b5
hobozhang <hongbo.zhang@uwaterloo.ca>
2014-12-21 00:42:36 -0500

    remove time module, add date module

---
+++ date.py
@@ -0,0 +1,54 @@
+class Date(object):
+	def __init__(self, year, month, day, hour, minute):
+		self.year = year
+		self.month = month
+		self.day = day
+		self.hour = hour
+		self.minute = minute
+
+	def __eq__(self, other):
+		return self.year == other.year\
+				and self.month == other.month\
+				and self.day == other.day\
+				and self.hour == other.hour\
+				and self.minute == other.minute
+
+	def __ne__(self, other):
+		return not(self == other)
+
+	def __le__(self, other):
+		if(self.year != other.year):
+			return self.year < other.year
+		elif(self,month != other.month):
+			return self.month < other.month
+		elif(self.day != other.day):
+			return self.day < other.day
+		elif(self.hour != other.hour):
+			return self.hour < other.hour
+		else:
+			return self.minute < other.minute
+
+	def __sub__(self, other):
+		return self.minute - other.minute\
+				+ (self.hour - other.hour) * 60\
+				+ (self.day - other.day) * 24 * 60\
+				+ (self.month - other.month) * days_in_month(year, month) * 24 * 60\
+				+ (self.year - other.year) * (356 + is_leap(year)) * 24 * 60
+
+def day_in_month(year, month):
+	if(month == 2): 
+		return 28 + is_leap(year)
+	elif(month in [4, 6, 9, 11]):
+		return 30
+	else:
+		return 31
+
+def is_leap(year):
+	if(year % 400 == 0):
+		return 1
+	elif(year % 100 == 0):
+		return 0
+	elif(year % 4 == 0):
+		return 1
+	else:
+		return 0
