8bb1beab8c024ce2f8fcec46e6b639a2d5c03a0a
zihanchen <zihanchen.ca@gmail.com>
2015-10-03 22:38:27 -0400

    make_event_list update

--- scheduloo.py
+++ scheduloo.py
@@ -1,5 +1,6 @@
 import event
 import datetime
+import evaluation
 
 class Scheduloo:
 	def __init__(self, db):
@@ -40,27 +41,32 @@ class Scheduloo:
 	
 	def make_event_list(self, start_date, end_date, time_schedule): #{{{
 		study_days = []
-		event_list = []	
-		# get weekdays that have courses
-		weekly_class = time_schedule[0]
-		for section in weekly_class:
-			for weekdays in section[0]:
-				if not weekdays in study_days:
-					study_days.append(weekdays)
-		study_days.sort()
-		# separating the weekly class into specific date
-		now = start_date
-		i = 0
-		while now <= end_date:
-			today = now.isoweekday()
-			if (today in study_days):
-				for section in weekly_class:
-					if (today in section[0]):
-						event_list.append([now, section[1], section[2]])
-				incre = (study_days[i + 1] - study_days[i] + 7) % 7
-				now += datetime.timedelta(days = incre)
-			else: now += datetime.timedelta(days = 1)
-		i = (i + 1) % len(study_days)
+		event_list = []
+		if (time_schedule == []): return event_list
+		if not (time_schedule[0] == []):
+			# get weekdays that have courses
+			weekly_class = time_schedule[0]
+			for section in weekly_class:
+				for weekdays in section[0]:
+					if not weekdays in study_days:
+						study_days.append(weekdays)
+			study_days.sort()
+			# separating the weekly class into specific date
+			now = start_date
+			i = 0
+			while now <= end_date:
+				today = now.isoweekday()
+				if (today in study_days):
+					for section in weekly_class:
+						if (today in section[0]):
+							event_list.append([now, section[1], section[2]])
+					incre = 0
+					if (len(study_days) > 1):
+						incre = (study_days[i + 1] - study_days[i] + 7) % 7
+					else: incre = 1
+					now += datetime.timedelta(days = incre)
+				else: now += datetime.timedelta(days = 1)
+			i = (i + 1) % len(study_days)
 		# adding the one-time courses
 		if (len(time_schedule) > 1):
 			event_list = event_list + time_schedule[1]
@@ -97,7 +103,7 @@ class Scheduloo:
 							self.courses[i][0],
 							self.courses[i][1],
 							section)
-					self.make_event_list(
+					print evaluation.evaluate(self.make_event_list(
 							datetime.date(2015, 9, 14),
 							datetime.date(2015, 12, 4),
-							time_schedule)
+							time_schedule))
